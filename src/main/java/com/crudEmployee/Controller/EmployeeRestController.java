20ed2623e927ba48000a915850d68efa51e64471
Rajesh2723 <hemannaparvathig@gmail.com>
2024-09-15 16:39:14 +0530

    codes

--- src/main/java/com/crudEmployee/Controller/EmployeeRestController.java
+++ src/main/java/com/crudEmployee/Controller/EmployeeRestController.java
@@ -2,8 +2,15 @@ package com.crudEmployee.Controller;
 
 import java.util.List;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +34,49 @@ public class EmployeeRestController {
 		
 		return employeeService.findAll();
 	}
+	
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		
+	Employee theEmployee=employeeService.findById(employeeId);
+	
+	if(theEmployee==null) {
+		throw new RuntimeException("Employee Id not found:--" +employeeId);
+	}
+		
+		return theEmployee;
+		
+	}
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+		
+		theEmployee.setId(0);
+		
+		Employee adbEmployee=employeeService.save(theEmployee); 
+		return adbEmployee;
+		
+	}
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
+		Employee dbEmployee=employeeService.save(theEmployee);
+		return theEmployee;
+	}
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+		
+//		System.out.println("Data deleted !!");
+//		employeeService.deleteById(employeeId);
+		Employee theEmployee=employeeService.findById(employeeId);
+		
+		if(theEmployee==null) {
+			throw new RuntimeException("Employee Id not found--" +employeeId);
+		}
+		employeeService.deleteById(employeeId);
+		
+		return "Deleted Employee Id:" +employeeId;
+		
+	}
+	
+ 
 	
 }
--- src/main/java/com/crudEmployee/CrudEmployeeApplication.java
+++ src/main/java/com/crudEmployee/CrudEmployeeApplication.java
@@ -9,5 +9,5 @@ public class CrudEmployeeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudEmployeeApplication.class, args);
 	}
-
+	
 }
